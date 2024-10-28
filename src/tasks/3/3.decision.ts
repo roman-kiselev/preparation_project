b7f24e9725851a55db617e33d588e1060788d789
Roman_Home <t89875041999@yandex.ru>
2024-10-28 23:42:00 +0300

    update

--- src/tasks/3/3.decision.ts
+++ src/tasks/3/3.decision.ts
@@ -6,5 +6,31 @@
  */
 
 export function calculateCoins(amount: number) {
-    const result: { [key: string]: number } = {};
+    const coins = {
+        "1": 0,
+        "2": 0,
+        "5": 0,
+        "10": 0,
+    };
+
+    while (amount > 0) {
+        if (amount >= 10) {
+            coins["10"]++;
+            amount -= 10;
+        } else if (amount >= 5) {
+            coins["5"]++;
+            amount -= 5;
+        } else if (amount >= 2) {
+            coins["2"]++;
+            amount -= 2;
+        } else {
+            coins["1"]++;
+            amount -= 1;
+        }
+    }
+    const result = Object.entries(coins).filter(([, value]) => value > 0);
+    const resultTwo = new Map(result).entries();
+    return resultTwo;
 }
+
+console.log(calculateCoins(123));
